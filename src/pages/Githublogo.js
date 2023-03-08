386ef1e74ae62ece112895a6e377ecb265016982
Ju Takeshi <lemalpro513@gmail.com>
2023-03-08 19:00:41 +0100

    version 1.0

--- src/pages/Githublogo.js
+++ src/pages/Githublogo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 function Gitlogo() {
-    const url = "https://github.com/Ju-Takeshi/Portfolio";
+    const url = "https://github.com/Ju-Takeshi/Portfolio-MMI.git";
     
     return (
       <Link to={url} target="_blank" rel="noreferrer">
