fdb7c399b0c72b3388e0a5534e39c8b4f831d655
Ju Takeshi <lemalpro513@gmail.com>
2023-03-09 02:42:05 +0100

    version 1.1

--- src/pages/Cvjulienlemal.js
+++ src/pages/Cvjulienlemal.js
@@ -11,7 +11,7 @@ const Cvjulienlemal = () => {
 
 export default Cvjulienlemal;
 
-const routes = [ { path: "/cvlemaljulien,", label: "CV"}];
+const routes = [ { path: "/cvlemaljulienmmi,", label: "CV"}];
 
 const header = document.querySelector("header");
 const nav = document.createElement("nav");
@@ -20,7 +20,7 @@ const ul = document.createElement("ul");
 routes.forEach(route => {
   const li = document.createElement("li");
   const a = document.createElement("a");
-  a.href = "./cvlemaljulien.pdf";
+  a.href = "./cvlemaljulienmmi.pdf";
   a.target= "_blank";
   a.textContent = route.label;
   li.appendChild(a);
