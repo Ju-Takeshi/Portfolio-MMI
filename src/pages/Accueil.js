ed86d8fbb67084b152bbaecf31172ce4d0a2adba
Ju Takeshi <lemalpro513@gmail.com>
2023-03-09 21:21:39 +0100

    version 1.2

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,27 +1,33 @@
 import React from "react";
 import Lml from "./Lml";
 import Footer from "./Footer";
+import Sakuraright from "./Sakuraright";
+import Sakuraleft from "./Sakuraleft";
 
 
 const Accueil = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", minHeight: "90vh" }}>
-      <h1 style={{ display: "flex", wordSpacing: "2px", color:"#a41514", marginLeft: "30px"}}>
-        Bienvenue sur mon portfolio ! Je m'apelle Julien et je souhaites reprendre mes études à l'IUT de Lens.
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", minHeight: "85vh" }}>
+      <h1 style={{ display: "flex", wordSpacing: "2px", color:"#a41514"}}>
+        Bienvenue sur mon portfolio. 
       </h1>
       <div style={{ maxWidth: "1600px"}}>
-      <p style={{display: "flex", fontSize: "28px", wordSpacing: "2px", color:"#white", marginLeft: "30px"}}>
-          L'informatique et la technologie ont toujours été une certaines partie de moi.<br /> 
-          Oui, en effet je suis passionné par ces domaines. Issu d'un BAC Sciences et Technologies de l'Industrie et du Developpement Durable, j'ai l'intention de reprendre les études plus précisément en BUT informatique,
-          car cette formation m'intéresse énormément. Il y a beaucoup de choses à découvrir, je sais pertinament que le developpement web saura m'épanouir !
+      <p style={{display: "flex", fontSize: "25px", wordSpacing: "2px", color:"#white", marginLeft: "30px"}}>
+          Le design et l'informatique ont toujours été une certaines partie de moi.<br /> 
+          En effet je suis passionné par ces domaines. Issu d'un BAC Sciences et Technologies de l'Industrie et du Developpement Durable, j'ai l'intention de reprendre les études plus précisément en BUT Métiers du Multimédia et de l'Internet,
+          car cette formation m'intéresse énormément. Il y a beaucoup de choses à découvrir, je sais pertinament que cette formation saura m'épanouir !
         </p>
         <p style={{fontSize: "28px", wordSpacing: "2px", color:"#white", marginLeft: "30px"}}>
           Je vous remercie d'ores et déjà pour votre visite sur ma page web portfolio et vous souhaites une bonne lecture !
         </p>
       </div>
       <div 
-        style={{ display: "flex", width:"50vh",}}>
+        style={{ display: "flex", width:"62vh",}}>
         <Lml />
+      </div>
+      <div>
+        <Sakuraright/>
+        <Sakuraleft/>
       </div>
       <footer 
   style={{
@@ -33,11 +39,9 @@ const Accueil = () => {
     bottom: 0,
     width: "100%",
     height: "9vh" /* Utilisation de l'unité de mesure "vh" pour que la hauteur soit relative à la hauteur de la fenêtre */
-  }}
->
+  }}>
   <Footer/>
 </footer>
-
     </div>
   );
 };
