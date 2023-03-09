1d00b47ba9eb7b1c563ba084237c0d37633310be
Ju Takeshi <lemalpro513@gmail.com>
2023-03-09 23:47:50 +0100

    version 1.0

---
+++ src/pages/Welcomeneon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./neonwelcome.css";
+
+const Welcomeneon = () => {
+    return (
+      <div className="welcome-neon">
+      <h1> Bienvenue sur mon portfolio.</h1>
+      </div>
+      );
+  }
+
+  export default Welcomeneon;
