30d05d68091b6395e3d2395f3134926d6510b5ca
Ju Takeshi <lemalpro513@gmail.com>
2023-03-09 21:30:03 +0100

    version 1.2

--- src/pages/Jpo.js
+++ src/pages/Jpo.js
@@ -14,8 +14,8 @@ const Jpo = () => {
         <p style={{fontSize: "25px", wordSpacing: "2px", color:"#white"}}>
           Le 4 février j'ai eu la chance de pouvoir découvrir l'IUT de Lens !<br></br>
           Ce jour-là, les membres du club Japon m'ont offert un accueil plus que chaleureux en m'invitant à participer aux activités mises en place par leur propre soin.<br></br>
-          J'ai également eu le droit à une présentation de l'établissement plus précisément la partie BUT / MMI mais aussi à plusieurs rencontres avec : Mme. Membre , Mr. Condotta , Mme. Saillot.<br></br>   
-          J'ai donc profité de leur disponibilité afin d'approfondir mes renseignements concernant la formation BUT informatique afin d'avoir une réelle idée de ce qui pouvait m'attendre.
+          J'ai également eu le droit à une présentation de l'établissement plus précisément la partie INFO / MMI.<br></br>   
+          Leur disponibilité m'a permis d'approfondir mes renseignements concernant la formation BUT MMI afin d'avoir une réelle idée de ce qui pouvait m'attendre.
         </p>
       </div>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", maxWidth: "900px"}}>
@@ -27,7 +27,7 @@ const Jpo = () => {
         </h1>
         <p style={{fontSize: "25px", wordSpacing: "2px", color:"#white"}}>
           Si je devais donner mon avis concernant cette journée, je pense très certainement que je dirai que ce fut une journée porte ouverte très surprenante dû à sa qualité d'accueil mais aussi de renseignements.<br></br>
-          Les rencontres que j'ai pu faire m'ont données encore plus envie d'être l'un des étudiants en BUT informatique de l'iut de Lens.<br></br> 
+          Les rencontres que j'ai pu faire m'ont données encore plus envie d'être l'un des étudiants en BUT MMI de l'iut de Lens.<br></br> 
           Car pour moi la qualité d'un établissement scolaire vient des professeurs mais également des élèves. Il n'y a rien de plus important qu'un entourage sain.<br></br>
           Donc oui cette journée était vraiment extraordinaire ! Moi aussi je souhaites faire partie du club japon et surtout étudier à l'iut de Lens. Je ferai tout ce qui est en mon possible pour y parvenir.
         </p>
