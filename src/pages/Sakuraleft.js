8093121538e5541856f1a6d57906ce1e9be37c4f
Ju Takeshi <lemalpro513@gmail.com>
2023-03-11 14:59:42 +0100

    version 1.1

--- src/pages/Sakuraleft.js
+++ src/pages/Sakuraleft.js
@@ -3,8 +3,10 @@ import "./sakuraleft.css";
 
 const sakuraleft = () => {
   return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", maxWidth: "900px", marginRight: "60vw", marginTop:"-125vh" }}>
     <div className="sakura-left">
     <img src="sakuraleft.png" alt="déco sakura gauche" />  
+    </div>
     </div>
     );
 }
