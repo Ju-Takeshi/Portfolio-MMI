279f8552da036220a3310a38636e7f680f0a9b74
Ju Takeshi <lemalpro513@gmail.com>
2023-03-11 14:59:12 +0100

    version 1.2

--- src/pages/Lml.js
+++ src/pages/Lml.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Lml = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", maxWidth: "900px"}}>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", maxWidth: "700px"}}>
     <img src="lml.png" alt="photo de moi" />  
     </div>
     );
