4cd38581193fe8813af9788ee316b603146d6ae5
Ju Takeshi <lemalpro513@gmail.com>
2023-03-11 15:00:18 +0100

    version 1.1

--- src/pages/Sakuraright.js
+++ src/pages/Sakuraright.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./sakuraright.css";
 
+
 const Sakuraright = () => {
   return (
+    <div style={{ display: "auto", justifyContent: "center", alignItems: "center", maxWidth: "900px", marginRight: "-80vw"}}>
     <div className="sakura-right">
     <img src="sakuraright.png" alt="déco sakura droite" />  
+    </div>
     </div>
     );
 }
