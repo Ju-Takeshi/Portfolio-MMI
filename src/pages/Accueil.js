b2be2b7d8a91061ee4099d5ef70558f42580e7ec
Ju Takeshi <lemalpro513@gmail.com>
2023-03-11 14:57:53 +0100

    version 1.4

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -8,27 +8,28 @@ import Welcomeneon from "./Welcomeneon";
 
 const Accueil = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", minHeight: "85vh" }}>
-      <h1 style={{ display: "flex", wordSpacing: "2px", color:"#a41514", marginTop: "-10px"}}>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", minHeight: "80vh" }}>
+      <h1 style={{ display: "flex", wordSpacing: "2pw", color:"#a41514", marginTop: "-3vh"}}>
         <Welcomeneon/>
       </h1>
       <div style={{ maxWidth: "1600px"}}>
-      <p style={{display: "flex", fontSize: "25px", wordSpacing: "2px", color:"#white", marginLeft: "30px", marginTop: "-10px"}}>
+      <p style={{display: "flex", fontSize: "25px", wordSpacing: "2px", color:"#white", marginLeft: "5vw", marginTop: "0vh"}}>
           Le design et l'informatique ont toujours été une certaines partie de moi.<br /> 
           En effet je suis passionné par ces domaines. Issu d'un BAC Sciences et Technologies de l'Industrie et du Developpement Durable, j'ai l'intention de reprendre les études plus précisément en BUT Métiers du Multimédia et de l'Internet,
           car cette formation m'intéresse énormément. Il y a beaucoup de choses à découvrir, je sais pertinament que cette formation saura m'épanouir !
         </p>
-        <p style={{fontSize: "28px", wordSpacing: "2px", color:"#white", marginLeft: "30px"}}>
+        <p style={{fontSize: "25px", wordSpacing: "2px", color:"#white", marginLeft: "5vw"}}>
           Je vous remercie d'ores et déjà pour votre visite sur ma page web portfolio et vous souhaites une bonne lecture !
         </p>
       </div>
-      <div 
-        style={{ display: "flex", width:"62vh",}}>
+      <div>
         <Lml />
       </div>
       <div>
-        <Sakuraright/>
         <Sakuraleft/>
+      </div>
+      <div>
+        <Sakuraright/>
       </div>
       <footer 
   style={{
@@ -48,7 +49,6 @@ const Accueil = () => {
 };
 
 export default Accueil;
-
 
 const routes = [ { path: "/home", label: "Accueil"}];
 
